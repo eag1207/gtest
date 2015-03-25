109f7ca8157d253ce9b2f8e7cb920d2064e29a54
Eric Gunn <eric@gunntechnologies.com>
2015-03-25 01:20:21 -0500

    created main file
    created and compiles main file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int num1 = 10;
+	int num2 = 5;
+	int count = 6;
+
+	for (int i = 0; i < count; ++i)
+	{
+		/* code */
+		printf("%d + %d = %d\n", num1, num2, num1+num2);
+	}
+}
